d18bf330c8d3170f55d9d880b5c70bb915651e9d
Stanislaw Halik <sthalik@misaki.pl>
2014-09-06 03:02:06 -0700

    build fix

--- utils/aruco_test.cpp
+++ utils/aruco_test.cpp
@@ -28,7 +28,6 @@ or implied, of Rafael Muñoz Salinas.
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <opencv2/core/utility.hpp>
 #include "aruco.h"
 #include "cvdrawingutils.h"
 using namespace cv;
