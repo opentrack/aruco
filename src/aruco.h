5a6e15b048adef8f6771222fcd87205548145e53
Stanislaw Halik <sthalik@misaki.pl>
2017-03-22 06:02:37 +0100

    fix build

--- src/aruco.h
+++ src/aruco.h
@@ -130,5 +130,5 @@ For testing the applications, the library provides videos and the corresponding 
 
 #include "markerdetector.h"
 //#include "boarddetector.h"
-#include "cvdrawingutils.h"
+//#include "cvdrawingutils.h"
 
